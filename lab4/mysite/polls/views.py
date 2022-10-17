f29cbc126492ee477f4092f22edd98b8587ad82a
Mateo <paezball@ualberta.ca>
2022-10-17 03:04:36 -0600

    Ready for lab6

--- lab4/mysite/polls/views.py
+++ lab4/mysite/polls/views.py
@@ -5,7 +5,7 @@ from django.views import generic
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
-    model = Question
+    template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
